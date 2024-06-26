7fe7b6149a984d8bcc40fb5c7b6c38264a8d46e8
Uwe Ritzschke <19396121+uritzschke@users.noreply.github.com>
2024-06-26 16:09:39 +0200

    added query generation for child | query

--- src/database/db.ts
+++ src/database/db.ts
@@ -13,28 +13,6 @@ const pool = mariaDb.createPool({
   trace: true,
 });
 
-async function asyncFunction() {
-  console.log("asyncFunction");
-  let conn: mariaDb.PoolConnection | undefined;
-  try {
-    conn = await pool.getConnection();
-    const rows = await conn.query("SELECT * from table1");
-    console.log(rows); //[ {val: 1}, meta: ... ]
-    /* const res = await conn.query("INSERT INTO myTable value (?, ?)", [
-            1,
-            "mariadb",
-            ]);
-            console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-            */
-  } catch (err) {
-    console.log("catch-Block");
-    throw err;
-  } finally {
-    console.log("finally-Block");
-    if (conn) return conn.end();
-  }
-}
-
 export default async function runSQLStatement(statement: string) {
   let conn: mariaDb.PoolConnection | undefined;
   try {
--- src/routes/queryDatabase.ts
+++ src/routes/queryDatabase.ts
@@ -1,9 +1,64 @@
-import { resourceLimits } from "worker_threads";
 import runSQLStatement from "../database/db";
 import Query from "../schema/query";
-import { resolveUrl } from "ajv/dist/compile/resolve";
+import { In, Equal } from "../schema/filter";
 
 export default async function queryDatabase(query: Query) {
-  let resultPromise: Promise<string> = runSQLStatement("SELECT * from table1");
+  let queryString: string = buildQueryString(query);
+
+  let resultPromise: Promise<string> = runSQLStatement(queryString);
   return resultPromise;
+}
+
+// This is a javascript/ typescript challenge: We can't simply get the type of an interface with "instanceof".
+// Instead, we have to check for an identifying parameter in the interface to determine it's type.
+function instanceOfIn(object: any): object is In {
+  return "values" in object;
+}
+
+function getCommaSeperatedValues(values: string[]) {
+  let ret: string = "";
+  values.forEach((value) => {
+    if (ret != "") {
+      ret += ", ";
+    }
+    ret += "'" + value + "'";
+  });
+
+  return ret;
+}
+
+function buildFilterString(filters: (In | Equal)[]) {
+  let filterString: string = "";
+  filters.forEach((filter) => {
+    if (filterString != "") {
+      filterString += " AND ";
+    }
+    if (instanceOfIn(filter)) {
+      filterString +=
+        filter.column +
+        " " +
+        filter.type +
+        " (" +
+        getCommaSeperatedValues(filter.values) +
+        ")";
+    } else {
+      filterString += filter.column + " = '" + filter.value + "'";
+    }
+  });
+
+  return filterString;
+}
+
+function buildQueryString(query: Query) {
+  let string =
+    "SELECT " + getCommaSeperatedValues(query.select) + " from " + query.table;
+
+  let filterString = buildFilterString(query.filters);
+
+  if (filterString != "") {
+    string += " WHERE " + filterString;
+  }
+
+  console.log("Query: " + string);
+  return string;
 }
